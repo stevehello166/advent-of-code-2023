a9c62d9ec34c692ee9adbb1a4fc6dbb1bcc64c24
stevehello166 <stevehello166@gmail.com>
2023-12-04 21:38:38 -0700

    solved day 2 on december 4 D:

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,11 +1,37 @@
-use std::{fs::File, io::{self, BufRead, BufReader, Lines}, path::Path};
+use std::{fs::File, io::{self, BufRead, BufReader, Lines}, path::{Path, self, PathBuf}};
+mod day2tests;
 
-const RED: u32 = 12;
-const GREEN: u32 = 13;
-const BLUE: u32 = 14;
+
+const RED: u32 = 13;
+const GREEN: u32 = 14;
+const BLUE: u32 = 15;
 
 fn main () -> io::Result<()> {
-    let file = File::open("assets/day2.txt")?;
+    //pt_1();
+    println!("{}", pt_2("assets/day2.txt".into()));
+    Ok(())
+}
+
+fn get_numbers(
+    colourvec: Vec<String>
+) -> Vec<u32>{
+    let mut  colour_collect: Vec<u32> = Vec::new();
+    for string in colourvec.clone() {
+        let mut collection_string = String::new();
+        for char in string.chars(){
+            if char.is_ascii_digit() {
+                collection_string.push(char);
+            }
+        }
+        let collection_num:u32 = collection_string.parse().expect("msg");
+        colour_collect.push(collection_num);
+        
+    }
+    return colour_collect;
+}
+
+fn pt_1 () {
+    let file = File::open("assets/day2.txt").expect("failed to read file");
     let reader = BufReader::new(file);
 
     let mut valid_games: Vec<u32> = Vec::new();
@@ -48,30 +74,60 @@ fn main () -> io::Result<()> {
                 }
             }
         }
-        
-        //println!("{:?}", green_collect[0]);
         game += 1;
     }
     
-    println!("{:?}", valid_games.iter().sum::<u32>());
+    println!("PART ONE{:?}", valid_games.iter().sum::<u32>());
     
-    Ok(())
 }
 
-fn get_numbers(
-    colourvec: Vec<String>
-) -> Vec<u32>{
-    let mut  colour_collect: Vec<u32> = Vec::new();
-    for string in colourvec.clone() {
-        let mut collection_string = String::new();
-        for char in string.chars(){
-            if char.is_ascii_digit() {
-                collection_string.push(char);
-            }
+fn pt_2(filepath: PathBuf) -> u32{
+    let file = File::open(filepath).expect("failed to read file");
+    let reader = BufReader::new(file);
+
+    let mut responsevec:Vec<u32> = Vec::new();
+    let mut game = 1;
+    for line in reader.lines() {
+        let line = line.expect("msg");
+        let splitvec:Vec<&str> = line.split(';').collect();
+
+        let mut redvec: Vec<String> = Vec:: new();
+        let mut greenvec: Vec<String> = Vec:: new();
+        let mut bluevec: Vec<String> = Vec:: new();
+        for string in splitvec {
+            let quartervec: Vec<&str> = string.split(',').collect();
+            for cstring in quartervec {
+                
+                if cstring.contains("red") {
+                    redvec.push(cstring.to_string());
+                    println!("{}",cstring)
+                } else if cstring.contains("green") {
+                    greenvec.push(cstring.to_string());
+                } else if cstring.contains("blue") {
+                    println!("{}",cstring);
+                    bluevec.push(cstring.to_string());
+                    
+                }
+            }  
         }
-        let collection_num:u32 = collection_string.parse().expect("msg");
-        colour_collect.push(collection_num);
+        let mut red_collect = get_numbers(redvec);
+        let mut green_collect = get_numbers(greenvec);
+        let mut blue_collect = get_numbers(bluevec);
+
+        red_collect.sort();
+        red_collect.reverse();
+        println!("highest red {}", red_collect[0]);
+        green_collect.sort();
+        green_collect.reverse();
+        println!("highest green {}", green_collect[0]);
+        blue_collect.sort();
+        blue_collect.reverse();
+        println!("highest blue {}", blue_collect[0]);
         
+        responsevec.push(red_collect[0] * green_collect[0] * blue_collect[0]);
+        println!("{}", red_collect[0] * green_collect[0] * blue_collect[0]);
     }
-    return colour_collect;
-}
+
+    
+    return(responsevec.iter().sum::<u32>());
+}   
---
+++ src/bin/day2tests/mod.rs
@@ -0,0 +1,8 @@
+use super::*;
+
+#[test]
+fn test_pt2() {
+    let val = pt_2("assets/day2_test.txt".into());
+    println!("{}", val);
+}
+
